6ec003f6c2e4ca4b09e214e96d3c35348dd4c874
VHPzin <vanderjr29@gmail.com>
2024-10-18 23:58:59 -0300

    telas_mobile

--- Site/templates/chat.js
+++ Site/templates/chat.js
@@ -6,14 +6,14 @@ window.addEventListener('DOMContentLoaded', function() {
     function adjustChatButtonPosition() {
         var scrollPosition = window.scrollY;
         if (window.innerWidth <= 1020) {
-            // Para telas de até 1020px, o botão de chat para antes do formulário "Fale Conosco"
+            
             if (scrollPosition >= formTop - window.innerHeight) {
                 chatButton.style.bottom = (scrollPosition - formTop + window.innerHeight + 20) + 'px';
             } else {
                 chatButton.style.bottom = '20px';
             }
         } else {
-            // Para telas maiores que 1020px, o botão de chat para no rodapé
+            
             if (scrollPosition >= footerTop - window.innerHeight) {
                 chatButton.style.bottom = (scrollPosition - footerTop + window.innerHeight + 20) + 'px';
             } else {
